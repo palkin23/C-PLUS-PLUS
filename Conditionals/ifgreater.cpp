63ceb8f270a3cf2251c35137c85b18bac06ecf3e
palkin23 <palkingupts23@gmail.com>
2026-01-07 23:48:32 +0530

    second if question

---
+++ Conditionals/ifgreater.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter a :"<<endl;
+    cin>>a;
+    cout<<"enter b:"<<endl;
+    cin>>b;
+    if(a>b){
+        cout<<"a is greater than b"<<endl;
+    }
+    else{
+        cout<<"b is greater than a"<<endl;
+
+    }
+}
