e39422a13c4b083997a63f14118749073d215529
palkin23 <palkingupts23@gmail.com>
2026-03-27 23:09:08 +0530

    change

--- Arrays/linearsearch.cpp
+++ Arrays/linearsearch.cpp
@@ -3,10 +3,10 @@ using namespace std;
 bool search(int arr[] , int size , int key){
     for (int i=0; i<size ; i++){
         if(arr[i]==key){
-            return 1;
+            return true;
         }
         else{
-            return 0;
+            return false;
         }
         }
 
