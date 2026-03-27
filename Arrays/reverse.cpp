0c4aed4f10c7b66e03bcf5fb9e34e7174fce2948
palkin23 <palkingupts23@gmail.com>
2026-03-27 23:35:57 +0530

    Array Reversed

--- Arrays/reverse.cpp
+++ Arrays/reverse.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
+void array1(int arr[] , int n){
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
 void reverse(int arr[] , int n){
     int start=0;
     int end=n-1;
@@ -9,5 +15,19 @@ void reverse(int arr[] , int n){
         end-- ;
      
 }
-
+void printarr2(int arr[] , int n){
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[5]={1,2,3,4,5};
+    cout<<"Original array is "<<endl;
+    array1(arr,5);
+    reverse(arr,5);
+    cout<<"Reversed array is "<<endl;
+    printarr2(arr,5);
+}
+return 0;
 }
