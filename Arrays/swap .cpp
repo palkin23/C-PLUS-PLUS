f9615a17d2227d87aad80b7716432e1eb0e912ed
palkin23 <palkingupts23@gmail.com>
2026-03-29 23:57:03 +0530

    swapped

---
+++ Arrays/swap .cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[6]={1,3,2,7,11,8};
+    cout<<"Original array is "<<endl;
+    for (int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for (int i=0;i<i+1;i++){
+        swap(arr[i],arr[i+1]);
+
+    }
+    cout<<"Swapped array is "<<endl;
+    for (int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+return 0;
+
+}
