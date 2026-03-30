b245c797889bba9dbd1e428ad76fc1dfcf9115e6
palkin23 <palkingupts23@gmail.com>
2026-03-30 23:16:08 +0530

    swapped alternate

--- Arrays/swap.cpp
+++ Arrays/swap.cpp
@@ -9,8 +9,10 @@ void printarr(int arr[], int size){
 //We are swapping alternate elements of the array
 void swap(int arr[] , int size){
     for(int i=0;i<size;i+=2){
+        if(i+1<size){
         swap(arr[i],arr[i+1]);
     }
+}
 }
 int main(){
     int even[8] = {1,2,3,4,5,6,7,8};
