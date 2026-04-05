4a137becca25049af0043ce21fcc9a72b80fcaca
palkin23 <palkingupts23@gmail.com>
2026-04-05 16:49:58 +0530

    Binary search Implementation

---
+++ Arrays/binarysearch.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+bool binarysearch(int arr[],int size , int key){
+    int start=0;
+    int end= size-1;
+    int mid=start+(end-start)/2;
+    while(start<=end){
+        if(aarr[mid]==key){
+            return mid;
+        }
+        if(key>arr[mid]{
+            start=mid+1; // moving to the right side
+        }
+        else{
+            end=mid-1: // moving to the left side
+        }
+        mid=start+(end-start)/2;
+
+        }
+        return -1; // element is not found in the array
+    }
+int main(){
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int key;
+    cin>>key;
+    int found=binarysearch(arr,10,key);
+    if(found!=-1){
+        cout<<"Element is found at index: "<<found<<endl;
+    }
+    else{
+        cout<<"Element is not found in array"<<endl;
+    }
+   
+}
