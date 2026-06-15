e17b3d5ca1da5acbae129d67816fd70c6788e398
palkin23 <palkingupts23@gmail.com>
2026-06-15 15:37:20 +0530

    new string

---
+++ Arrays/string.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string p="Palkin";
+    int len=p.size();
+    cout<<"Length of string is "<<len<<endl;
+    cout<<p[len-1]<<endl; // to print last character of the string
+    return 0;
+}
