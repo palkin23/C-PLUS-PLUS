fe1596f95d90dd16261bb08df50b8d0b29ea9b7f
palkin23 <palkingupts23@gmail.com>
2026-06-15 16:11:16 +0530

    pair

---
+++ stl/pair.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int ,pair<int , int>>p={1,{2,3}};
+    cout<<p.first<<" "<<p.second.first<<" "<<p.second.second<<endl;
+    return 0;
+}
