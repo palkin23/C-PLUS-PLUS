d2c65ad9ec739cc4918f84660a0b8ce7ed9bbaaf
palkin23 <palkingupts23@gmail.com>
2026-06-17 13:58:08 +0530

    p10

---
+++ Conditionals/Patterns/p10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        //space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<5-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
