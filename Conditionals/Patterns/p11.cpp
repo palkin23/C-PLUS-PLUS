eb7f48a2684ab4057ac6487a866ef1f8f935ff09
palkin23 <palkingupts23@gmail.com>
2026-06-18 11:47:58 +0530

    p11

---
+++ Conditionals/Patterns/p11.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i=0;i<5;i++){
+        //space
+        for(int j=0;j<=4-i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<=2*i+1;j++){
+            cout<<"*";
+        
+        }
+        cout<<endl;
+    }
+}
