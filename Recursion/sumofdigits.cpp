ea9aafd86e5577fd269254633e32a1034c9fc93a
palkin23 <palkingupts23@gmail.com>
2026-06-18 12:27:57 +0530

    sum

---
+++ Recursion/sumofdigits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int sum(int n){
+    if (n>=0 && n<10){
+        return n;
+    }
+    else{
+        return n%10+sum(n/10);
+    }
+
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"Sum of digits is "<<sum(n)<<endl;
+}
