f343cfac31b3bf81e92b88aa1d764a39982eccc5
palkin23 <palkingupts23@gmail.com>
2026-06-25 13:32:38 +0530

    Hourglass Pattern

---
+++ Conditionals/Patterns/hourglass_hollow.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=9;i++){
+        for(int j=1;j<=9;j++){
+            if(i==j || i+j==10 || i==1||i==9){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+    cout<<endl;
+    }
+}
