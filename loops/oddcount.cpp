8790ece0741582ef9b7702e0c09679a6345b3847
palkin23 <palkingupts23@gmail.com>
2026-06-28 15:31:07 +0530

    odd no's counted from 1 to n

---
+++ loops/oddcount.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+    cin>>n;
+    int count=0;
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            count=count+1;
+        }
+    }
+    cout<<"Count of odd no's is : "<<count<<endl;
+    return 0;
+}
