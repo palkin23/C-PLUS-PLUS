74d3428527c75aef9d947ac1f4a8761570ae2d3d
palkin23 <palkingupts23@gmail.com>
2026-06-28 14:58:16 +0530

    1 to n

---
+++ loops/1ton.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+     cout<<" no's from 1 to n"<<endl;
+for(int i=1;i<=n;i++){
+       cout<<" "<<i<<endl;
+        }
+    return 0;
+    }
