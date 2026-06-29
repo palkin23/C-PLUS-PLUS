f469ea57f0c76813439d966ded672b678ad7978f
palkin23 <palkingupts23@gmail.com>
2026-06-29 14:46:39 +0530

    Fibonacci Series printed

--- loops/FIBONACCI.cpp
+++ loops/FIBONACCI.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int num1=0;
+    int num2=1;
+    int num3=num1+num2;
     int n;
     cout<<"Enter a number: ";
     cin>>n;
-    cout<<endl;
-    int i;
-    if(i<=2){
-        return 1;
+    for(int i=num1;i<n;i++){
+        cout<<num1<<endl;
+        num1=num2;
+        num2=num3;
+        num3=num1+num2;
     }
+return 0;
 }
