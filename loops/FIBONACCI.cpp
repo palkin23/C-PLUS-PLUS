4232b0a27ebdbb2e34b09171e72d7b5181c3237b
palkin23 <palkingupts23@gmail.com>
2026-06-29 14:42:01 +0530

    Prime no's from 1 to 100

---
+++ loops/FIBONACCI.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<endl;
+    int i;
+    if(i<=2){
+        return 1;
+    }
+}
