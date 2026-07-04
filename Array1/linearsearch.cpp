98e56e01bf2383f31b9d289db96d1a565ebf83e8
palkin23 <palkingupts23@gmail.com>
2026-07-04 11:52:17 +0530

    linear search

--- Array1/linearsearch.cpp
+++ Array1/linearsearch.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int arr[5]={5,4,3,4,2};
+int main()
+{
+    int arr[5] = {5, 4, 3, 4, 2};
     int key;
-    cout<<"Enter the element you want to find"<<endl;
-    cin>>key;
-    for(int i=0;i<5;i++){
-        if(arr[i]==key){
-            cout<<"Element is found at index : "<<i+1<<endl;
-           // break;
+    cout << "Enter the element you want to find" << endl;
+    cin >> key;
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] == key)
+        {
+            cout << "Element is found at index : " << i + 1 << endl;
+            // break;
         }
     }
-    //else
-            cout<<"Not found!"<<endl;
-
 }
