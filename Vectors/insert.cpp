012a519163b17820e868b736e39d7f81076791ac
palkin23 <palksgupta23@gmail.com>
2026-07-22 17:12:58 +0530

    Insert in vectors

---
+++ Vectors/insert.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec={1,2,3,4,5};
+    vec.insert(vec.begin()+3,99);
+    for(int v:vec){
+        cout<<v<<" ";
+    }
+return 0;
+
+}
