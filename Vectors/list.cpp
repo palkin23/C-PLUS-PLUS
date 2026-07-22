11c94653bee96b41f2f107799651d954ff225bfe
palkin23 <palksgupta23@gmail.com>
2026-07-22 17:26:04 +0530

    list

--- Vectors/list.cpp
+++ Vectors/list.cpp
@@ -1,20 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    list<string>cars={"Mercedes","BMW","Ford"};
-    cout<<cars.front();
-    cout<<endl;
-    cout<<cars.back();
-    cout<<endl;
+int main()
+{
+    list<string> cars = {"Mercedes", "BMW", "Ford"};
+    cout << cars.front();
+    cout << endl;
+    cout << cars.back();
+    cout << endl;
     cars.push_front("abc");
     cars.push_back("z");
-    for(string v:cars){
-        cout<<v<<" ";
+    for (string v : cars)
+    {
+        cout << v << " ";
     }
-    cout<<endl;
+    cout << endl;
     cars.pop_front();
     cars.pop_back();
-for(string v:cars){
-        cout<<v<<" ";
+    for (string v : cars)
+    {
+        cout << v << " ";
     }
+    cout << endl;
 }
