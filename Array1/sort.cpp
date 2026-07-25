aeff350997d06424a2c2019fe5a7ab1d8abf4485
palkin23 <palksgupta23@gmail.com>
2026-07-26 00:46:52 +0530

    Sort in Vectors

---
+++ Array1/sort.cpp
@@ -0,0 +1,10 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr={1,4,9,5,8};
+   sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            cout<<arr[i]<<" ";
+}
+return 0;
+}
