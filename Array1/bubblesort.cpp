eaaaebc19939bc637f1eb7074a167d7f12fbfa8a
palkin23 <palksgupta23@gmail.com>
2026-07-26 00:43:27 +0530

    BUBBLE SORT

--- Array1/bubblesort.cpp
+++ Array1/bubblesort.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int arr[5] = {3, 2, 8, 1, 5};
-    for (int i = 0; i < 5; i++)
-    { for(int k=0;k<5;k++){
-        int j = i + 1;
-        if (arr[i] > arr[j])
-        {
-            arr[i] = arr[j];
-            j++;
+
+void bubbleSort(int arr[], int n) {
+
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
         }
+
+        if (!swapped)
+            break;
     }
 }
 
-    // to print the array
-    for (int i = 0; i < 5; i++)
-    {
-        cout << " " << arr[i];
+int main() {
+
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
+
+    return 0;
 }
